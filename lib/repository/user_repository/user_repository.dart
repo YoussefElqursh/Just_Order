ae869f3fae4a744714e55e4b519a24698ce61df6
ziad-ezzat <ezzatziad83@gmail.com>
2024-08-03 20:14:21 +0200

    add more loguc in user repo

--- lib/repository/user_repository/user_repository.dart
+++ lib/repository/user_repository/user_repository.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:just_order/models/item_model.dart';
 import 'package:just_order/models/restaurant_model.dart';
 
 class UserRepository {
@@ -7,6 +8,25 @@ class UserRepository {
 
   UserRepository({FirebaseFirestore? firestore})
       : _firestore = firestore ?? FirebaseFirestore.instance;
+
+// home screen //
+  Future<List<String>> getAllAdvertisements(String code) async {
+    final snapshot = await _firestore.collection('advertisements').get();
+    List<String> advertisements = [];
+    for (var doc in snapshot.docs) {
+      advertisements.add(doc.data()['imageURL']);
+    }
+    return advertisements;
+  }
+
+  Future<List<Item>> getPobularItems(String code) async {
+    // logic to get popular items ****************
+    List<Item> items = [];
+    // for (var doc in snapshot.docs) {
+    //   items.add(Item.fromMap(doc.data()));
+    // }
+    return items;
+  }
 
   Future<List<Restaurant>> getAllAccessRestaurants(String code) async {
     final snapshot = await _firestore.collection('restaurants')
