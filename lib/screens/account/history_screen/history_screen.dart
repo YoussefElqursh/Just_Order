b857d4b6ed7bf16416142b04bb33e91d462ade24
youssef883 <79144849+youssef883@users.noreply.github.com>
2024-12-15 12:35:55 +0200

    add history module

--- lib/screens/account/history_screen/history_screen.dart
+++ lib/screens/account/history_screen/history_screen.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
-import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:just_order/models/enums/status.dart';
+import 'package:just_order/models/order_model.dart';
 import 'package:just_order/models/restaurant_model.dart';
 import 'package:just_order/models/user_model.dart';
 import 'package:just_order/repository/order_provider.dart';
@@ -11,7 +11,6 @@ import 'package:just_order/shared/function/functions.dart';
 import 'package:just_order/shared/widget/common_order_state_widget.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 
 class HistoryScreen extends StatefulWidget {
   const HistoryScreen({super.key});
@@ -24,6 +23,8 @@ class _HistoryScreenState extends State<HistoryScreen> {
   final OrderRepository _orderRepository = OrderRepository();
   Map<String, Restaurant> restaurantMap = {};
   User? user;
+  bool _isOrdersLoaded = false;
+  bool _isLoading = true;
 
   @override
   void initState() {
@@ -35,34 +36,56 @@ class _HistoryScreenState extends State<HistoryScreen> {
     final prefs = await SharedPreferences.getInstance();
     final userJson = prefs.getString('user');
     if (userJson != null) {
+      final loadedUser = User.fromJson(jsonDecode(userJson));
       setState(() {
-        user = User.fromJson(jsonDecode(userJson));
+        user = loadedUser;
       });
+      _getOrders();
     }
-    _getOrders();
   }
 
   void _getOrders() async {
+    if (_isOrdersLoaded) return;
+    _isOrdersLoaded = true;
+
+    setState(() {
+      _isLoading = true;
+    });
+
     final fetchedOrders = await _orderRepository.getOrders(user?.userId ?? '');
-    final restaurantIds =
-        fetchedOrders.map((order) => order.restaurantId).toList();
-    final fetchedRestaurants =
-        await _orderRepository.getRestaurants(restaurantIds);
+    final restaurantIds = fetchedOrders.map((order) => order.restaurantId).toList();
+    final fetchedRestaurants = await _orderRepository.getRestaurants(restaurantIds);
+
     setState(() {
       restaurantMap = {
         for (var restaurant in fetchedRestaurants)
           restaurant.restaurantId: restaurant
       };
+      _isLoading = false;
     });
-    // ignore: use_build_context_synchronously
+
     Provider.of<OrderProvider>(context, listen: false)
         .listenToOrders(user?.userId ?? '');
   }
 
+  List<Order> getFilteredOrders(List<Order> orders) {
+    return orders.where((order) {
+      return [
+        Status.delivered,
+        Status.declined,
+        Status.cancelled,
+        Status.autoDeclined,
+        Status.finalized
+      ].contains(order.status);
+    }).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Consumer<OrderProvider>(builder: (context, orderProvider, child) {
-      final orders = orderProvider.orders;
+    return _isLoading
+        ? Scaffold(body: Center(child: CircularProgressIndicator(color: const Color(0xFFE02C45),)))
+        : Consumer<OrderProvider>(builder: (context, orderProvider, child) {
+      final orders = getFilteredOrders(orderProvider.orders);
 
       return Scaffold(
         appBar: AppBar(
@@ -78,104 +101,49 @@ class _HistoryScreenState extends State<HistoryScreen> {
             overflow: TextOverflow.ellipsis,
             maxLines: 1,
           ),
-          leading: Padding(
-            padding: const EdgeInsets.only(left: 20.0, top: 10.0, bottom: 10.0),
-            child: Container(
-              width: 34,
-              height: 34,
-              clipBehavior: Clip.antiAlias,
-              decoration: ShapeDecoration(
-                color: const Color(0xFFF4F4F4),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8)),
-              ),
-              child: IconButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                icon: const Icon(
-                  Icons.arrow_back,
-                  color: Colors.black,
-                  size: 18,
-                ),
-              ),
-            ),
+          leading: IconButton(
+            onPressed: () => Navigator.pop(context),
+            icon: const Icon(Icons.arrow_back, color: Colors.black),
           ),
         ),
-        body: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          physics: const BouncingScrollPhysics(),
-          child: Container(
-            width: MediaQuery.sizeOf(context).width,
-            padding: const EdgeInsets.all(20),
-            child: Builder(
-              builder: (context) {
-                if (orders
-                    .where((order) => order.status == Status.pending)
-                    .isEmpty) {
-                  return Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          setPhoto(
-                            kind: 0,
-                            path: 'assets/images/order.png',
-                            height: 100,
-                            width: 100,
-                          ),
-                          const SizedBox(
-                            height: 15,
-                          ),
-                          Text(
-                            'No History Orders',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 12,
-                              fontFamily: 'Inter',
-                              fontWeight: FontWeight.w600,
-                            ),
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 1,
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                } else {
-                  return ListView.separated(
-                    itemBuilder: (context, index) =>
-                        buildOrderDeliveredStateWidget(
-                            context: context,
-                            order: orders
-                                .where(
-                                    (order) => order.status == Status.delivered)
-                                .toList()[index],
-                            width: 70,
-                            restaurant: restaurantMap[orders
-                                    .where((order) =>
-                                        order.status == Status.delivered)
-                                    .toList()[index]
-                                    .restaurantId] ??
-                                Restaurant.empty()),
-                    separatorBuilder: (context, index) => const SizedBox(
-                      height: 12.0,
-                    ),
-                    itemCount: min(
-                        3,
-                        orders
-                            .where((order) => order.status == Status.delivered)
-                            .length),
-                    scrollDirection: Axis.vertical,
-                    physics: const BouncingScrollPhysics(),
-                    shrinkWrap: true,
-                  );
-                }
-              },
-            ),
+        body: orders.isEmpty
+            ? Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              setPhoto(
+                kind: 0,
+                path: 'assets/images/order.png',
+                height: 100,
+                width: 100,
+              ),
+              const SizedBox(height: 15),
+              const Text(
+                'No History Orders',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 12,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
           ),
+        )
+            : ListView.separated(
+          itemBuilder: (context, index) {
+            final order = orders[index];
+            final restaurant = restaurantMap[order.restaurantId] ?? Restaurant.empty();
+            return buildOrderDeliveredStateWidget(
+              context: context,
+              order: order,
+              restaurant: restaurant,
+              width: 70,
+            );
+          },
+          separatorBuilder: (context, index) => const SizedBox(height: 12),
+          itemCount: orders.length,
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
         ),
       );
     });
